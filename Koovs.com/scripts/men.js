c1fd38be159c47e05455c414b4ff3c379f41004f
pragati tripathi <mailpragatihere@gmail.com>
2022-04-02 17:16:23 +0530

    completed linked pages

---
+++ Koovs.com/scripts/men.js
@@ -0,0 +1,262 @@
+var menData = [
+    {
+        name: "Perfect Fit Muscle Fit Full Sleeve T-Shirt",
+        price : 899,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/A07A0736_aa8b947c-1943-42bb-971d-1141a7c25f50_720x.jpg?v=1646804480",
+        category: "T-shirts",
+        publishDate: 2021,
+    },
+    {
+        name: "Bright White Casual Wear Solid Tee",
+        price : 699,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/A07A0037_360x.jpg?v=1646804480",
+        category: "T-shirts",
+        publishDate: 2020,
+    },
+    {
+        name: "Muscle Fit Full Sleeve T-Shirt",
+        price : 899,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/A07A0839_1f01348a-54c4-48c5-8248-d99b39ac8b27_720x.jpg?v=1646804478",
+        category: "T-shirts",
+        publishDate: 2021,
+    },
+    {
+        name: "Muscle Fit Full Sleeve T-Shirt",
+        price : 899,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/A07A0354_720x.jpg?v=1646804479",
+        category: "T-shirts",
+        publishDate: 2022,
+    },
+    {
+        name: "Casual Wear Basic Crew Pink Tee",
+        price: 250,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/A07A1980_720x.jpg?v=1640347833",
+        category: "T-shirts",
+        publishDate: 2022,
+    },
+    {
+        name: "win Patch Pocket Denim Jacket",
+        price: 1019,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/155086_df3b7c8fc642433b862d8e4dd09e1dcd_image1_zoom_720x.jpg?v=1629455927",
+        category: "Jackets",
+        publishDate: 2022,
+    },
+    {
+        name: "One/Zero By Koovs Zip-Up Windproof Track Jacket",
+        price: 800,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/163196_c03ae51a21b841c99a5637fe73144431_image1_zoom_720x.jpg?v=1631124154",
+        category: "Jackets",
+        publishDate: 2019,
+    },
+    {
+        name: "One/Zero By Koovs Contrast Panel Windproof Tracksuit",
+        price: 1120,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/163217_95e5737b56384ba7a8705a91aff7d527_image1_zoom_720x.jpg?v=1644218296",
+        category: "Jackets",
+        publishDate: 2021,
+    },
+    {
+        name: "One/Zero By Koovs Colour Block Track Jacket",
+        price: 800,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/165303_7bec7744642a46b8980869acf04d0a4a_image1_zoom_720x.jpg?v=1631124391",
+        category: "Jackets",
+        publishDate: 2021,
+    },
+    {
+        name: "Blue Saint Zipper Closure Jacket",
+        price: 800 ,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/158434_21b97f514d53452d9666e4e5b995f878_image1_zoom_720x.jpg?v=1631122793",
+        category: "Jackets",
+        publishDate: 2022,
+    },
+    {
+        name: "Dark Wash Skinny Jeans",
+        price: 599,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/158440_82a971f7379f4da9b97e695b2ad4a272_image1_zoom_720x.jpg?v=1631101307",
+        category: "Denim",
+        publishDate: 2021,
+    },
+    {
+        name: "Light Wash Slim Jeans",
+        price: 599,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/158438_4881c934195b43eaa84821a2b9d453ff_image1_zoom_720x.jpg?v=1631101203",
+        category: "Denim",
+        publishDate: 2021,
+    },
+    {
+        name: "Realm Men Olive Blue Slim Fit Jeans",
+        price: 440,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/IMG_2850_720x.jpg?v=1634749593",
+        category: "Denim",
+        publishDate: 2020,
+    },
+    {
+        name: "Dark-Wash Skinny Fit Jeans",
+        price: 599,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/158477_16d3d276ec0246ce8f21e4d750f06ac5_image1_zoom_720x.jpg?v=1636354382",
+        category: "Denim",
+        publishDate: 2020,
+    },
+    {
+        name: "Mid Rise Graphic-Print Jeans",
+        price: 1150,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/167320_a72257f5a7fc4976967810617e1ab5ab_image1_zoom_720x.jpg?v=1629890787",
+        category: "Denim",
+        publishDate: 2022,
+    },
+    {
+        name: "Men'S Blue Slim Fit Casual Shirt",
+        price: 600,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/155457_947f19abac8446daa56f7e1a3c83c24d_image1_zoom_35dc6419-536d-4b3d-ae15-efa12240be4b_720x.jpg?v=1627489684",
+        category: "shirts",
+        publishDate: 2022,
+    },
+    {
+        name: "Basic Long Sleeve Shirt",
+        price: 400,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/154990_45b4db70a9e4440faa655479db372ea7_image1_zoom_9969320f-7788-4020-a050-ecee283246e3_720x.jpg?v=1627490613",
+        category: "shirts",
+        publishDate: 2020,
+    },
+    {
+        name: "Classic Floral Print Cuban Collar Shirt",
+        price: 520,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/166785_9d09aeb5096b4880a5f8a9895b828a1f_image1_zoom_f9050c15-b875-43a8-a37f-143ba6e87261_720x.jpg?v=1644216726",
+        category: "shirts",
+        publishDate: 2019,
+    },
+    {
+        name: "Floral Placement Print Casual Shirt",
+        price: 320,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/158280_10a2102ae52d4067a166892af2982dc7_image1_zoom_720x.jpg?v=1645528078",
+        category: "shirts",
+        publishDate: 2021,
+    },
+    {
+        name: "Multi Floral Print Cuban Collar Shirt",
+        price: 520,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/166786_f13187e91c754ac297b992f4dcada08f_image1_zoom_720x.jpg?v=1631093496",
+        category: "shirts",
+        publishDate: 2022,
+    },
+    {
+        name: "Pink Tropical Print Flip Flops",
+        price: 240,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/1_fb7533fc-8eb8-485e-9458-5dfbdaa3cb49_720x.jpg?v=1634749585",
+        category: "footwear",
+        publishDate: 2021,
+    },
+    {
+        name: "Sun Print Flip Flops",
+        price: 240,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/1_57202746-f821-4afa-a525-745b148f8871_720x.jpg?v=1634749584",
+        category: "footwear",
+        publishDate: 2021,
+    },
+    {
+        name: "Sun Print Flip Flops",
+        price: 300,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/1_52a20d32-ab53-4390-825c-d941515113ed_720x.jpg?v=1634749584",
+        category: "footwear",
+        publishDate: 2018,
+    },
+    {
+        name: "Glitter Strap Flip Flops",
+        price: 500,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/1_413cecd5-6702-433a-a2c4-cb36d00e74dd_720x.jpg?v=1634749582",
+        category: "footwear",
+        publishDate: 2020,
+    },
+    {
+        name: "Strawberry Print Flip Flops",
+        price: 400,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0584/9399/3141/products/1_8cc4df25-0b3d-4b0d-9bfe-f7962d6490d3_720x.jpg?v=1634749584",
+        category: "footwear",
+        publishDate: 2022,
+    },
+];
+
+var cartProducts = JSON.parse(localStorage.getItem("cartData")) || [];
+
+displayData(menData);
+
+function displayData(menData) {
+    menData.map(function (elem) {
+        var product = document.createElement("div");
+        product.setAttribute("class","productBox");
+
+        var image = document.createElement("img");
+        image.src = elem.imgUrl;
+        image.setAttribute("class","productImg");
+
+        var pName = document.createElement("h4");
+        pName.innerText = elem.name;
+
+        var pPrice = document.createElement("p");
+        pPrice.innerText = 'Rs.' + elem.price;
+
+        var btn1 = document.createElement("button");
+        btn1.innerText = 'Add to Cart';
+        btn1.addEventListener("click",function(){
+            addToCart(elem);
+        });
+
+        var btn2 = document.createElement("button");
+        btn2.innerText = 'Add to MyList';
+
+        product.append(image, pName, pPrice, btn1, btn2);
+
+        document.querySelector(".products").append(product);
+    });
+}
+
+function sortByPrice() {
+    document.querySelector(".products").innerHTML = "";
+    var selected = document.querySelector("#sort").value;
+    if (selected == 'lth')
+    {
+        menData.sort(function (a,b) {
+            return a.price - b.price;
+        });
+        displayData(menData);
+    }
+    if (selected == 'htl')
+    {
+        menData.sort(function (a,b) {
+            return b.price - a.price;
+        });
+        displayData(menData);
+    }
+    if (selected == 'nto')
+    {
+        menData.sort(function (a,b) {
+            return b.publishDate - a.publishDate;
+        });
+        displayData(menData);
+    }
+}
+
+function specificProduct() {
+    document.querySelector(".products").innerHTML = "";
+    
+    var selected = document.querySelector("#category").value;
+
+    var filteredList = menData.filter(function (elem) {
+            return elem.category == selected;
+          });
+          displayData(filteredList);
+}
+
+function addToCart(elem) {
+    console.log(elem);
+    cartProducts.push(elem);
+    localStorage.setItem("cartData",JSON.stringify(cartProducts));
+}
+
+
+
+
+
+
+
+
